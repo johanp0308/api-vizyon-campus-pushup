41e21d6cac829370a26cdcbfc90527ddf78dddca
johanp0308 <sebastian-johan1@hotmail.com>
2024-04-23 08:49:28 -0500

    feat: :tada: Init Commit

---
+++ src/main/java/com/vizyon/api/appz/AppZApplication.java
@@ -0,0 +1,13 @@
+package com.vizyon.api.appz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppZApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppZApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vizyon/api/appz/AppZApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vizyon.api.appz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppZApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
